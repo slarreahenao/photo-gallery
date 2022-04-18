cf168c69ba3613ecacc18d41251a76c1c03b6f12
Santiago Larrea <santiago.larrea@pragma.com.co>
2022-04-17 21:42:47 -0500

    chore: Config project.

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // The root of your source code, typically /src
+  // `<rootDir>` is a token Jest substitutes
+  roots: ['<rootDir>/src'],
+
+  // Jest transformations -- this adds support for TypeScript
+  // using ts-jest
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+
+  // Test spec file resolution pattern
+  // Matches parent folder `__tests__` and filename
+  // should contain `test` or `spec`.
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+
+  // Module file extensions for importing
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+  // Files to ignore
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+    '.+\\.(svg)$': '<rootDir>/__mocks__/svgrMock.js'
+  },
+  testPathIgnorePatterns: [`node_modules`, `.cache`, `public`],
+
+  testEnvironment: 'jsdom'
+}
